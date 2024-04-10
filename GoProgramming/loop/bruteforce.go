c15c34b216ede1805c4e65f32251ee1a6e0cd2a7
Hasan Hüseyin Aygar <hsn.aygr.6@gmail.com>
2024-04-11 00:03:22 +0300

    bruteforce.go

--- GoProgramming/loop/bruteforce.go
+++ GoProgramming/loop/bruteforce.go
@@ -1,13 +1,10 @@
 package loop
-import "fmt"
 
-func FindBruteForceAlgorithm(setOfArray []int, number int) {
-	var iterationNumber int = 1
+func FindBruteForceAlgorithm(setOfArray []int, number int) bool {
 	for i := 0; i < len(setOfArray); i++ {
-
 		if setOfArray[i] == number {
-			fmt.Println("Researched number: ", setOfArray[i], " ", "Iteratıon number: ", iterationNumber)
+			return true
 		}
-		iterationNumber++
 	}
-}
+	return false
+}}
